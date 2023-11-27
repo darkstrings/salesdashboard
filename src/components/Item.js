07e0965da432aa19c791f9e44373caf00c444446
Lucien Gaydos <darkstrings@gmx.com>
2023-11-27 15:50:15 -0500

    merge error fix

--- src/components/Item.js
+++ src/components/Item.js
@@ -6,19 +6,15 @@ function Item({ item, dispatch }) {
         <li>Item: {item.name}</li>
         <li>Item price: ${item.price}</li>
         {item.splitNum > 0 ? <li>Split between {item.splitNum + 1} people</li> : ""}
-<<<<<<< HEAD
         <li>
           You made <span className="num-display">${item.commissionEarned}</span> commission from this sale
         </li>
-=======
-        <li>You made ${item.commissionEarned} commission from this sale</li>
->>>>>>> bee31ee3cd0a75699780758ee3f1039e5d94f804
         <div className="d-grid gap-2 d-md-block mx-auto">
           <button
             type="button"
             className="btn btn-outline-danger"
             onClick={() => {
-              dispatch({ type: "deleteItem", payload: item });
+              dispatch({ type: "deleteItem", payload: item.id });
             }}>
             Delete
           </button>
