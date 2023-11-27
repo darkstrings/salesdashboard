bee31ee3cd0a75699780758ee3f1039e5d94f804
L.Aerin <78057999+darkstrings@users.noreply.github.com>
2023-11-26 22:08:12 -0500

    Update Item.js
    Typo fix

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,7 +5,7 @@ function Item({ item, onDelete }) {
       <ul className="sold-list" key={item.id}>
         <li>Item: {item.name}</li>
         <li>Item price: ${item.price}</li>
-        {item.splitNum > 0 ? <li>Split between ${item.splitNum + 1} people</li> : ""}
+        {item.splitNum > 0 ? <li>Split between {item.splitNum + 1} people</li> : ""}
         <li>You made ${item.commissionEarned} commission from this sale</li>
         <div className="d-grid gap-2 d-md-block mx-auto">
           <button type="button" className="btn btn-outline-danger" onClick={() => onDelete(item)}>
